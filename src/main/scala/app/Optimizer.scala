2115a6fa7aa8386ee97d96ad97edb1c933c46005
RoelofRuis <roelof.ruis@gmail.com>
2019-05-10 09:43:27 +0200

    Cache per round, saves on memory

--- src/main/scala/app/Optimizer.scala
+++ src/main/scala/app/Optimizer.scala
@@ -6,30 +6,35 @@ object Optimizer {
 
   def findBest(initialSchedule: Schedule, score: ScoreFun, expand: ExpandFun): (Schedule, Score) = {
     val queue = mutable.Queue[Schedule]()
-    val seenSchedules = mutable.HashSet[Schedule]()
     var roundIndex = 1
     var bestSchedule = initialSchedule
     var bestScore: Score = score(initialSchedule)
 
     while (roundIndex < initialSchedule.size) {
       println(s"Optimizing round [${roundIndex + 1}]...")
+
+      val seenRounds = mutable.HashSet[Round]()
       queue.enqueue(bestSchedule)
+
       while (queue.nonEmpty) {
         println(s"Queue size [${queue.size}]")
         val currentSchedule = queue.dequeue()
         val currentRound = currentSchedule(roundIndex)
 
         expand(currentRound)
-          .map { newRound => currentSchedule.updated(roundIndex, newRound) }
+          .flatMap { newRound =>
+            if (seenRounds.contains(newRound)) List()
+            else {
+              seenRounds.add(newRound)
+              List(currentSchedule.updated(roundIndex, newRound))
+            }
+          }
           .foreach { schedule =>
-            if ( ! seenSchedules.contains(schedule)) {
-              seenSchedules.add(schedule)
-              val scored = score(schedule)
-              if (scored > bestScore) {
-                bestScore = scored
-                bestSchedule = schedule
-                queue.enqueue(schedule)
-              }
+            val scored = score(schedule)
+            if (scored > bestScore) {
+              bestScore = scored
+              bestSchedule = schedule
+              queue.enqueue(schedule)
             }
           }
       }
