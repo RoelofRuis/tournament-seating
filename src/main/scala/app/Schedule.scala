c69bcb74b1c0a8ac42e46168e409a4ae824dc5f6
RoelofRuis <roelof.ruis@gmail.com>
2019-05-14 22:23:52 +0200

    little speed improvement for the swap function

--- src/main/scala/app/Schedule.scala
+++ src/main/scala/app/Schedule.scala
@@ -10,21 +10,16 @@ object Schedule {
     Range.inclusive(1, nRounds).map { _ => tableVector }
   }
 
-  def swapPlayers(round: Round, player1: Int, player2: Int): Round = {
-    var target = round
-    val indexedTarget = target.zipWithIndex
-    val p1 = indexedTarget(player1)
-    val p2 = indexedTarget(player2)
-    Seq((p1._1, p2._2), (p2._1, p1._2)).foreach { case (table, pos) => target = target.updated(pos, table) }
-
-    target
-  }
-
   def expandAll: ExpandFun = { round =>
     val indexedRound: Seq[(TableSize, Int)] = round.zipWithIndex
-    indexedRound.flatMap { case (table, p1) =>
-      indexedRound.filter { case (t, p2) => t != table && p2 > p1 }
-        .map { case (_, p2) => swapPlayers(round, p1, p2) }
+
+    indexedRound.flatMap {
+      case (table, player1) =>
+        indexedRound
+          .filter { case (t, player2) => t != table && player2 > player1 }
+          .map {
+            case (_, player2) => round.updated(player2, round(player1)).updated(player1, round(player2))
+          }
     }
   }
 
